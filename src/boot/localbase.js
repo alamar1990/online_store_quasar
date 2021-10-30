1dab6ef6897e3bc72e31cf5eeec80b0791834886
Alain Marquez <alamar1990@gmail.com>
2021-10-30 12:26:17 -0400

    Refactor seeder

--- src/boot/localbase.js
+++ src/boot/localbase.js
@@ -10,98 +10,95 @@ export default boot(async ({app, router}) => {
   //Inject ProductService into Vue Global Instance
   app.config.globalProperties.productService = new ProductService(db)
 
-  const result = await app.config.globalProperties.$db.collection('config').get()
+  let dbConfig = await app.config.globalProperties.$db.collection('config').get()
 
-  if (result.length === 0 || !result[0].isSeeded) {
+  if (dbConfig.length === 0 || !dbConfig[0].isSeeded) {
     console.log('Running seeder')
     await app.config.globalProperties.$db.collection('product').set([
       {
         id: 1,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 1",
         image_url: "https://picsum.photos/150",
       },
       {
         id: 2,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 2",
         image_url: "https://picsum.photos/150",
       },
       {
         id: 3,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 3",
         image_url: "https://picsum.photos/150",
       },
       {
         id: 4,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 4",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 5,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 5",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 6,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 6",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 7,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 7",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 8,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 8",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 9,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 9",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 10,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 10",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 11,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 11",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 12,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 12",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 13,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 13",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 14,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 14",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 15,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 15",
         image_url: "~/assets/parallax2.jpg",
       },
       {
         id: 16,
-        text: "Lorem ipsum dolor sit amet,",
+        text: "Product 16",
         image_url: "~/assets/parallax2.jpg",
       },
     ])
-  }
 
-  // Set seeded flag for data seeding
-  if (result.length === 0) {
+    // Set seeded flag for data seeding
     await app.config.globalProperties.$db.collection('config').add({isSeeded: true})
   }
-
 
 })
